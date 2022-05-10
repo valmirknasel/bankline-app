2ca8051724f4160bd8e14bb80f3d0a39f2a10861
Valmir Ademar Weide Knäsel Júnior <valmirknasel@gmail.com>
2022-05-10 12:41:46 -0400

    criado o componente para criar uma nova movimentacao na api bankline-api

---
+++ src/app/compoents/movimentacao-new/movimentacao-new.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { CorrentistaService } from 'src/app/services/correntista.service';
+import { MovimentacaoService } from 'src/app/services/movimentacao.service';
+
+@Component({
+  selector: 'app-movimentacao-new',
+  templateUrl: './movimentacao-new.component.html',
+  styleUrls: ['./movimentacao-new.component.css']
+})
+export class MovimentacaoNewComponent implements OnInit {
+  correntistas: any;
+  correntistaSelecionado:any;
+
+  constructor(private movimentacaoService: MovimentacaoService, private correntistaService: CorrentistaService) { }
+
+  ngOnInit(): void {
+    this.listarCorrentistas();
+  }
+
+  listarCorrentistas(): void {
+    this.correntistaService.list().subscribe(
+      data => {
+        this.correntistas = data;
+        console.log(data);
+      },
+      error => {
+        console.log(error);
+      });
+  }
+
+}

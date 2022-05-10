da19e0663c066806eb0a44deee33ebeaccc90067
Valmir Ademar Weide Knäsel Júnior <valmirknasel@gmail.com>
2022-05-10 12:46:29 -0400

    criado o correntista.service para conectar na api /correntistas

---
+++ src/app/services/correntista.service.ts
@@ -0,0 +1,19 @@
+// services/correntista.service.ts
+
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+//URL base da api que vai ser consultada
+const baseUrl = 'http://localhost:8080';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CorrentistaService {
+  constructor(private http: HttpClient) { }
+  
+  list(): Observable<any> {
+    return this.http.get(`${baseUrl}/correntistas`);
+  }
+}

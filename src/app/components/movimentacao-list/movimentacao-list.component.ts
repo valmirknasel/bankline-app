848315f8c71476096914c8f062099fba84287c99
Valmir Ademar Weide Knäsel Júnior <valmirknasel@gmail.com>
2022-05-12 02:13:15 -0400

    tentativa de integrar os dados da api com o model

--- src/app/components/movimentacao-list/movimentacao-list.component.ts
+++ src/app/components/movimentacao-list/movimentacao-list.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { MovimentacaoService } from 'src/app/services/movimentacao.service';
+import {Component, OnInit} from '@angular/core';
+import {MovimentacaoService} from 'src/app/services/movimentacao.service';
 import {Movimentacao} from "../../model/Movimentacao";
+import {CorrentistaService} from "../../services/correntista.service";
+import {Correntista} from "../../model/Correntista";
 
 @Component({
   selector: 'app-movimentacao-list',
@@ -9,22 +11,40 @@ import {Movimentacao} from "../../model/Movimentacao";
 })
 export class MovimentacaoListComponent implements OnInit {
 
+  contadorTeste: number = 0;
   movimentacoes: Movimentacao[] = [];
+  correntistas: Correntista[] = [];
 
-  constructor(private movimentacaoService: MovimentacaoService) { }
-
-  ngOnInit(): void {
-    this.listMovimentacoes();
+  constructor(private movimentacaoService: MovimentacaoService, private correntistaService: CorrentistaService) {
   }
 
-  listMovimentacoes():void {
-    this.movimentacaoService.list().subscribe(data => {
-      this.movimentacoes = data;
-      console.log(data);
-    },
-    error => {
-      console.log(error);
+  ngOnInit(): void {
+    this.recuperaDadosDaApi();
+  }
+
+  recuperaDadosDaApi(): void {
+    this.contadorTeste++;
+    let ms = this.movimentacaoService.list();
+
+    ms.subscribe(listaDeMovimentacoes => {
+        this.movimentacoes = listaDeMovimentacoes;
+        console.log('dados da api de movimentacoes: ' + listaDeMovimentacoes);
+      },
+      error => {
+        console.log(error);
+      });
+    this.vinculaMovimentacaoAoCorrentista();
+    console.log('Contador de chamadas: ' + this.contadorTeste);
+  }
+
+  private vinculaMovimentacaoAoCorrentista() {
+    console.log('entrou no metodo vincula movimentacao');
+    this.movimentacoes.forEach(mov => console.log(mov.descricao));
+    let v = this.movimentacoes.map(function (mov: Movimentacao): string {
+      //TODO descobrir pq está vazio o mapeamento apesar da listar estar preenchida...
+      return mov.descricao
     })
+    console.log('movimentacao: ' + v);
   }
 
 }

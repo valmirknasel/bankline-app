dbc198b7dc17cb7b443673de79627b53e87ef3be
Valmir Ademar Weide Knäsel Júnior <valmirknasel@gmail.com>
2022-05-13 20:17:06 -0400

    concluidas as chamadas de teste para a api de movimentacoes e para a api de correntistas. Como teste,usei o metodo para recuperar os dados das duas apis e montar a tabela. O codigo foi limpo.

--- src/app/components/movimentacao-list/movimentacao-list.component.ts
+++ src/app/components/movimentacao-list/movimentacao-list.component.ts
@@ -2,7 +2,6 @@ import {Component, OnInit} from '@angular/core';
 import {MovimentacaoService} from 'src/app/services/movimentacao.service';
 import {Movimentacao} from "../../model/Movimentacao";
 import {CorrentistaService} from "../../services/correntista.service";
-import {Correntista} from "../../model/Correntista";
 
 @Component({
   selector: 'app-movimentacao-list',
@@ -11,41 +10,31 @@ import {Correntista} from "../../model/Correntista";
 })
 export class MovimentacaoListComponent implements OnInit {
 
-  contadorTeste: number = 0;
   movimentacoes: Movimentacao[] = [];
-  correntistas: Correntista[] = [];
 
   constructor(private movimentacaoService: MovimentacaoService, private correntistaService: CorrentistaService) {
   }
 
   ngOnInit(): void {
     this.recuperaDadosDaApi();
-    this.recuperaDadosDaApi2()
   }
 
-
   recuperaDadosDaApi(): void {
-
-    let ms = this.movimentacaoService.list();
-    ms.subscribe(listaDeMovimentacoes => {
+    //nao facam isso em producao que vai dar xabu
+    this.movimentacaoService.findAll().subscribe(listaDeMovimentacoes => {
+        listaDeMovimentacoes.forEach(movimentacao => {
+          this.correntistaService.getCorrentistaById(movimentacao.idCorrentista).subscribe(correntista => {
+            if (correntista !== null) {
+              if (movimentacao.idCorrentista === correntista.id) {
+                movimentacao.correntista = correntista;
+              }
+            }
+          })
+        })
         this.movimentacoes = listaDeMovimentacoes;
       },
       error => {
         console.log(error);
-      });
-  }
-
-  recuperaDadosDaApi2(): Movimentacao[] {
-    let movimentacoes: Movimentacao[] = [];
-    let ms = this.movimentacaoService.list();
-    ms.subscribe(listaDeMovimentacoes => {
-        movimentacoes = listaDeMovimentacoes;
-      },
-      error => {
-        console.log(error);
-      });
-    console.log('listaMovimentacoes: ' + movimentacoes);
-    console.log('tamanho listaMovimentacoes: ' + movimentacoes.length);
-    return movimentacoes;
+      })
   }
 }

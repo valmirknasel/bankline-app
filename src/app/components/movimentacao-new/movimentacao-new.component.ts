c9ada9e343031eff2948b426ae8e5f2dcfc39dd7
Valmir Ademar Weide Knäsel Júnior <valmirknasel@gmail.com>
2022-05-14 03:49:13 -0400

    criado o metodo salvar e alguns metodos auxiliares para testes

--- src/app/components/movimentacao-new/movimentacao-new.component.ts
+++ src/app/components/movimentacao-new/movimentacao-new.component.ts
@@ -1,32 +1,99 @@
-import { Component, OnInit } from '@angular/core';
-import { CorrentistaService } from 'src/app/services/correntista.service';
-import { MovimentacaoService } from 'src/app/services/movimentacao.service';
+import {Component, OnInit} from '@angular/core';
+import {CorrentistaService} from 'src/app/services/correntista.service';
+import {MovimentacaoService} from 'src/app/services/movimentacao.service';
 import {Correntista} from "../../model/Correntista";
+import {TipoMovimentacao} from "../../model/TipoMovimentacao";
+import {MovimentacaoDTO} from "../../model/dto/MovimentacaoDTO";
 
 @Component({
-  selector: 'app-movimentacao-new',
-  templateUrl: './movimentacao-new.component.html',
-  styleUrls: ['./movimentacao-new.component.css']
+	selector: 'app-movimentacao-new',
+	templateUrl: './movimentacao-new.component.html',
+	styleUrls: ['./movimentacao-new.component.css']
 })
 export class MovimentacaoNewComponent implements OnInit {
-  correntistas: Correntista[] = [];
-  correntistaSelecionado:any;
 
-  constructor(private movimentacaoService: MovimentacaoService, private correntistaService: CorrentistaService) { }
+	//tem que ser declarado assim para que possa usar o enum no componente.html
+	tipoMovimentacao = TipoMovimentacao;
+	correntistas: Correntista[] = [];
+	//correntista e movimentacao podem ficar undefined durante o carregamento da pagina, por isso se coloca o !
+	correntistaSelecionado!: Correntista;
+	movimentacao!: MovimentacaoDTO;
 
-  ngOnInit(): void {
-    this.listarCorrentistas();
-  }
 
-  listarCorrentistas(): void {
-    this.correntistaService.list().subscribe(
-      data => {
-        this.correntistas = data;
-        console.log(data);
-      },
-      error => {
-        console.log(error);
-      });
-  }
+	constructor(private movimentacaoService: MovimentacaoService, private correntistaService: CorrentistaService) {
+	}
 
+	ngOnInit(): void {
+		this.listarCorrentistas();
+		this.inicializaForm();
+	}
+
+	listarCorrentistas(): void {
+		this.correntistaService.list().subscribe(
+			data => {
+				this.correntistas = data;
+				//TODO remover as impressoes no log
+				console.log("listarCorrentistas");
+				console.log(data);
+			},
+			error => {
+				console.log(error);
+			});
+	}
+
+	salvarMovimentacao(): void {
+
+		//TODO remover as impressoes no log
+		console.log("salvarMovimentacao-movimentacao");
+		console.log(this.movimentacao);
+
+		if (this.movimentacao !== undefined) {
+			//TODO verificar por qual motivo os dados sao enviados fora de ordem apesar de chegar ate aqui na ordem correta
+			this.movimentacaoService.gravarMovimentacao(this.movimentacao).subscribe(
+				response => {
+					console.log("success");
+					console.log(response);
+				},
+				error => {
+					console.log("error");
+					console.log(error);
+				}
+			);
+		} else {
+			this.inicializaForm();
+			//TODO remover as impressoes no log
+			console.log("salvarMovimentacao-inicializarForm")
+		}
+	}
+
+	private inicializaForm() {
+		if (this.movimentacao === undefined) {
+			this.movimentacao = new MovimentacaoDTO('', 0, TipoMovimentacao.RECEITA, 0);
+		}
+	}
+
+	selecionaCorrentista() {
+		this.movimentacao.idCorrentista = this.correntistaSelecionado.id;
+		//TODO remover as impressoes no log
+		console.log("selecionaCorrentista")
+		console.log(this.movimentacao)
+	}
+
+	selecionaTipoMovimento() {
+		//TODO remover as impressoes no log
+		console.log("selecionaTipoMovimento")
+		console.log(this.movimentacao)
+	}
+
+	descricaoModificada() {
+		//TODO remover as impressoes no log
+		console.log("descricaoModificada")
+		console.log(this.movimentacao)
+	}
+
+	valorModificado() {
+		//TODO remover as impressoes no log
+		console.log("valorModificado")
+		console.log(this.movimentacao)
+	}
 }

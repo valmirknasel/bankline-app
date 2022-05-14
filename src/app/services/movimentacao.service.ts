c71a7bec99cc0459f34f50f2c2d12a8c9df6d9d4
Valmir Ademar Weide Knäsel Júnior <valmirknasel@gmail.com>
2022-05-13 20:18:59 -0400

    realizados ajustes e limpeza no codigo

--- src/app/services/movimentacao.service.ts
+++ src/app/services/movimentacao.service.ts
@@ -3,31 +3,18 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import {Movimentacao} from "../model/Movimentacao";
 
-//caminho base da api que vai ser acessada
-const baseUrl = 'http://localhost:8080'
-
 @Injectable({
   providedIn: 'root'
 })
 export class MovimentacaoService {
 
+  //caminho base da api que vai ser acessada
+  private baseUrl = "http://localhost:8080"
+  private listaDeMovimentacoesUrl = `${this.baseUrl}/movimentacoes`
+
   constructor(private http: HttpClient) { }
 
-    list(): Observable<any> {
-      return this.http.get(`${baseUrl}/movimentacoes`);
-    }
-
-    listaMovimentacoes(): Movimentacao[] {
-      let ms = this.list();
-      let movimentacoes: Movimentacao[] = [];
-      ms.subscribe(listaDeMovimentacoes => {
-          movimentacoes = listaDeMovimentacoes;
-        },
-      error => {
-        console.log(error);
-      });
-      console.log('dados da api de movimentacoes: ' + movimentacoes);
-      console.log('tamanho movimentacoes: ' + movimentacoes.length);
-      return movimentacoes;
+    findAll(): Observable<Movimentacao[]> {
+      return this.http.get<Movimentacao[]>(this.listaDeMovimentacoesUrl);
     }
 }

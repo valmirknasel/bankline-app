82473b2c74cde631617fd4092b48ef470e5ea7d9
Valmir Ademar Weide Knäsel Júnior <valmirknasel@gmail.com>
2022-05-14 03:52:42 -0400

    criado o metodo gravarMovimentacao para fazer o post na api. Necessario entender por qual motivo os dados sao enviados fora de ordem.

--- src/app/services/movimentacao.service.ts
+++ src/app/services/movimentacao.service.ts
@@ -1,20 +1,29 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
 import {Movimentacao} from "../model/Movimentacao";
+import {MovimentacaoDTO} from "../model/dto/MovimentacaoDTO";
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class MovimentacaoService {
 
-  //caminho base da api que vai ser acessada
-  private baseUrl = "http://localhost:8080"
-  private listaDeMovimentacoesUrl = `${this.baseUrl}/movimentacoes`
+	//caminho base da api que vai ser acessada
+	private baseUrl = "http://localhost:8080"
+	private listaDeMovimentacoesUrl = `${this.baseUrl}/movimentacoes`
 
-  constructor(private http: HttpClient) { }
+	constructor(private http: HttpClient) {
+	}
 
-    findAll(): Observable<Movimentacao[]> {
-      return this.http.get<Movimentacao[]>(this.listaDeMovimentacoesUrl);
-    }
+	findAll(): Observable<Movimentacao[]> {
+		return this.http.get<Movimentacao[]>(this.listaDeMovimentacoesUrl);
+	}
+
+	gravarMovimentacao(movimentacao: MovimentacaoDTO) {
+			//TODO remover as impressoes no log
+			console.log("gravarMovimentacao")
+			console.log(movimentacao)
+			return this.http.post(`${this.baseUrl}/movimentacoes`, movimentacao);
+	}
 }

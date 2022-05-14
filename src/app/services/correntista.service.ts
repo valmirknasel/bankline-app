f0e9f29ead52cbc03dc19498541e60e094c6605f
Valmir Ademar Weide Knäsel Júnior <valmirknasel@gmail.com>
2022-05-13 20:22:07 -0400

    ajustado o tipo de retorno do metodo.

--- src/app/services/correntista.service.ts
+++ src/app/services/correntista.service.ts
@@ -3,6 +3,7 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
+import {Correntista} from "../model/Correntista";
 
 //URL base da api que vai ser consultada
 const baseUrl = 'http://localhost:8080';
@@ -14,12 +15,12 @@ export class CorrentistaService {
   constructor(private http: HttpClient) {
   }
 
-  list(): Observable<any> {
-    return this.http.get(`${baseUrl}/correntistas`);
+  list(): Observable<Correntista[]> {
+    return this.http.get<Correntista[]>(`${baseUrl}/correntistas`);
   }
 
-  getCorrentistaById(idCorrentista: number): Observable<any> {
+  getCorrentistaById(idCorrentista: number): Observable<Correntista> {
     let id = idCorrentista;
-    return this.http.get(`${baseUrl}/correntistas/${id}`);
+    return this.http.get<Correntista>(`${baseUrl}/correntistas/${id}`);
   }
 }

54a89b9bb8eaedb624d6d70e669112b000d0bf26
Valmir Ademar Weide Knäsel Júnior <valmirknasel@gmail.com>
2022-05-13 20:23:48 -0400

    ajuste na importacao do LOCALE_ID

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -8,7 +8,6 @@ import { MovimentacaoNewComponent } from './components/movimentacao-new/moviment
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule} from '@angular/common/http';
-import { LOCALE_ID } from '@angular/core';
 import { registerLocaleData } from '@angular/common';
 import  localePt from '@angular/common/locales/pt';
 
